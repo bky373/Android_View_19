addd87225fedeb5fd14a31887865fe6bb9f6a3ea
bky373 <bo373@naver.com>
2019-12-14 17:55:05 +0900

    do #3-5 in ViewAndViewGroup

--- linearlayout/src/main/java/com/example/linearlayout/LinearMainActivity.java
+++ linearlayout/src/main/java/com/example/linearlayout/LinearMainActivity.java
@@ -2,7 +2,9 @@ package com.example.linearlayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LinearMainActivity extends AppCompatActivity {
 
@@ -10,5 +12,25 @@ public class LinearMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_linear_main);
+    }
+
+//    public void equal_button_click(View view) {
+//        Intent intent = new Intent(getApplicationContext(), EqualActivity.class);
+//        startActivity(intent);
+//    }
+//
+//    public void unequal_button_click(View view) {
+//        Intent intent = new Intent(getApplicationContext(), UnequalActivity.class);
+//        startActivity(intent);
+//    }
+
+    public void buttonClick(View view) {
+        Intent intent;
+        if (view.getId() == R.id.equal_button) {
+            intent = new Intent(getApplicationContext(), EqualActivity.class);
+        } else {
+            intent = new Intent(getApplicationContext(), UnequalActivity.class);
+        }
+        startActivity(intent);
     }
 }
