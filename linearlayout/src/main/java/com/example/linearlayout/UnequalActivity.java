a48536a00b8ee5dc5e8e7ff92476ffe9b2522932
bky373 <bo373@naver.com>
2019-12-14 17:10:54 +0900

    do #3-2 in ViewAndViewGroup

---
+++ linearlayout/src/main/java/com/example/linearlayout/UnequalActivity.java
@@ -0,0 +1,14 @@
+package com.example.linearlayout;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UnequalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_equal);
+    }
+}
