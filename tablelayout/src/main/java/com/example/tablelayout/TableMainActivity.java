a87e5d7cab0c7e411b04a8c11cd9f3277cee2af3
bky373 <bo373@naver.com>
2019-12-15 22:19:54 +0900

    do #6-1 in ViewAndViewGroup for Table_Layout

---
+++ tablelayout/src/main/java/com/example/tablelayout/TableMainActivity.java
@@ -0,0 +1,14 @@
+package com.example.tablelayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TableMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_table_main);
+    }
+}
