e2d932be7390ec3c400d2f88166a75888737da3f
bky373 <bo373@naver.com>
2019-12-16 23:35:46 +0900

    do #7-1 in ViewAndViewGroup for Constraint_Layout

---
+++ constraintlayout/src/main/java/com/example/constraintlayout/ConstraintMainActivity.java
@@ -0,0 +1,14 @@
+package com.example.constraintlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConstraintMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraint_main);
+    }
+}
